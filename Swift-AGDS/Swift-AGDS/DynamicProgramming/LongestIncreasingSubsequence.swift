bfd4bd781327fe80c336f341a844d56679b30647
Quien <quien697@gmail.com>
2023-04-26 00:26:10 -0700

    update Longest Increasing Subsequence

--- Swift-AGDS/Swift-AGDS/DynamicProgramming/LongestIncreasingSubsequence.swift
+++ Swift-AGDS/Swift-AGDS/DynamicProgramming/LongestIncreasingSubsequence.swift
@@ -19,8 +19,9 @@ import Foundation
  */
 func lengthOfLIS(_ nums: [Int]) -> Int {
   // Using Bottom-up Approach -> Tabulation + Memorization.
-  // recurrence
-  // d[n] = min(d[i], d[i - j * j] + 1) where j * j <= i
+  // Time complexity: O(N^2)
+  // Space complexity: O(N)
+  // Recurrence: d[n] =
   
   let n = nums.count
   var d = [Int](repeating: 1, count: n)
@@ -31,13 +32,58 @@ func lengthOfLIS(_ nums: [Int]) -> Int {
     while i > j {
       if nums[i] > nums[j] && d[j] >= d[i] {
         d[i] = d[j] + 1
-        if maxCount < d[i] {
-          maxCount = d[i]
-        }
+        maxCount = max(maxCount, d[i])
       }
       j += 1
     }
   }
-  print(d)
+  
   return maxCount
+}
+
+func lengthOfLIS2(_ nums: [Int]) -> Int {
+  // Using Bottom-up Approach -> Tabulation + Memorization. (Patience Sorting Algorithm)
+  // Time complexity: O(N^2)
+  // Space complexity: O(N)
+  // Recurrence: d[n] =
+  
+  var d = [Int]()
+  for num in nums {
+    if let j = d.firstIndex(where: { $0 >= num }) {
+      if num <= d[j] {
+        d[j] = num
+      }
+    } else {
+      d.append(num)
+    }
+  }
+  return d.count
+}
+
+func lengthOfLIS3(_ nums: [Int]) -> Int {
+  // Using Bottom-up Approach -> Tabulation + Memorization. (Patience Sorting Algorithm)
+  // Time complexity: O(NlgN)
+  // Space complexity:
+  // Recurrence: d[n] =
+  
+  var d = [Int]()
+  for num in nums {
+    // Using Binary Search
+    var lower = 0
+    var upper = d.count
+    while lower < upper {
+      let mid = (upper + lower) / 2
+      if d[mid] < num {
+        lower = mid + 1
+      } else {
+        upper = mid
+      }
+    }
+    if lower == d.count {
+      d.append(num)
+    } else {
+      d[lower] = num
+    }
+  }
+  return d.count
 }
